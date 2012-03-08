8c4003335e2fa5e5246400670d84a32a55de7b3e
Jon Bringhurst <jonb@lanl.gov>
2012-03-08 10:42:19 -0700

    Load user arguments into struct for later processing.

--- src/dcp.c
+++ src/dcp.c
@@ -14,6 +14,10 @@
 #include "copy.h"
 #include "filestat.h"
 
+#ifndef ARG_MAX
+    #define ARG_MAX _POSIX_ARG_MAX
+#endif
+
 /** The loglevel that this instance of dcopy will output. */
 DCOPY_loglevel DCOPY_debug_level;
 
@@ -160,6 +164,7 @@ void DCOPY_parse_path_args(char** argv, int optind, int argc)
     int index;
     size_t num_args = argc - optind;
     int last_arg_index = num_args + optind - 1;
+    char** dbg_p;
     
     if(argv == NULL || num_args < 2) {
         DCOPY_print_usage(argv);
@@ -169,15 +174,25 @@ void DCOPY_parse_path_args(char** argv, int optind, int argc)
     }
 
     /* The destination will always be the last item. */
-    DCOPY_user_opts.dest_path = (char*) malloc(PATH_MAX * sizeof(char));
+    DCOPY_user_opts.dest_path = (char*) malloc((PATH_MAX + 1) * sizeof(char));
     strncpy(DCOPY_user_opts.dest_path, argv[last_arg_index], PATH_MAX);
-    LOG(DCOPY_LOG_DBG, "Found a destination path with name: %s", DCOPY_user_opts.dest_path);
+
+    /* Now lets go back and get everything else for the source paths. */
+    DCOPY_user_opts.src_path = (char*) malloc((ARG_MAX + 1) * sizeof(char));
+    memset(DCOPY_user_opts.src_path, 0, (ARG_MAX + 1) * sizeof(char));
 
     for(index = optind; index < last_arg_index; index++) {
-        LOG(DCOPY_LOG_DBG, "Found a source path with name: %s", argv[index]);
+        DCOPY_user_opts.src_path[index - optind] = (char*) malloc((PATH_MAX + 1) * sizeof(char));
+        strncpy(DCOPY_user_opts.src_path[index - optind], argv[index], PATH_MAX * sizeof(char));
     }
 
-    exit(EXIT_SUCCESS);
+    /* Now we can print it out for debugging purposes. */
+    dbg_p = DCOPY_user_opts.src_path;
+    while(*dbg_p != NULL) {
+        LOG(DCOPY_LOG_DBG, "Found a source path with name: %s", *dbg_p);
+        *dbg_p++;
+    }
+    LOG(DCOPY_LOG_DBG, "Found a destination path with name: %s", DCOPY_user_opts.dest_path);
 }
 
 int main(int argc, char** argv)
