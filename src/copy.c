6cad6647bf4f1d69a234ce73798326a0da07c457
Jon Bringhurst <jonb@lanl.gov>
2012-03-12 11:36:34 -0600

    Minor clean for copy operation.

--- src/copy.c
+++ src/copy.c
@@ -1,5 +1,6 @@
 /* See the file "COPYING" for the full license governing this code. */
 
+#include <errno.h>
 #include <fcntl.h>
 #include <libgen.h>
 #include <limits.h>
@@ -24,72 +25,65 @@ extern DCOPY_statistics_t DCOPY_statistics;
 
 void DCOPY_do_copy(DCOPY_operation_t* op, CIRCLE_handle* handle)
 {
-    char newfile[PATH_MAX];
+    char new_file_path[PATH_MAX];
     char buf[DCOPY_CHUNK_SIZE];
-    char tmppath[PATH_MAX];
-    char* base_operand;
-    FILE* in;
 
-    LOG(DCOPY_LOG_DBG, "COPY - operand is `%s', subop is `%s', baseidx is `%d', dest is `%s'.", \
+    size_t bytes_read = 0;
+    size_t bytes_written = 0;
+
+    FILE* in;
+    int outfd;
+
+    LOG(DCOPY_LOG_DBG, "Copy, operand is `%s', subop is `%s', baseidx is `%d', dest is `%s'.", \
         op->operand, op->operand + op->base_index, op->base_index, DCOPY_user_opts.dest_path);
+    LOG(DCOPY_LOG_DBG, "Copy, `%s' chunk `%d'.", op->operand, op->chunk);
+
+    sprintf(new_file_path, "%s%s", DCOPY_user_opts.dest_path, op->operand + op->base_index);
 
     in = fopen(op->operand, "rb");
-
-    LOG(DCOPY_LOG_DBG, "Copy %s chunk %d", op->operand, op->chunk);
+    outfd = open(new_file_path, O_RDWR | O_CREAT, 00644);
 
     if(!in) {
-        LOG(DCOPY_LOG_ERR, "Unable to open %s", op->operand);
-        perror("open");
+        LOG(DCOPY_LOG_ERR, "Unable to open `%s'. %s", \
+            op->operand, strerror(errno));
         return;
     }
 
-    strncpy(tmppath, op->operand, PATH_MAX);
-    base_operand = basename(tmppath);
-
-    /**
-     * If we have a file grab the basename and append.
-     */
-    if(DCOPY_user_opts.merge_into_dest) {
-        sprintf(newfile, "%s%s", DCOPY_user_opts.dest_path, op->operand + op->base_index);
-    }
-    else {
-        sprintf(newfile, "%s%s/%s", DCOPY_user_opts.dest_path, op->operand + op->base_index, base_operand);
-    }
-
-    int outfd = open(newfile, O_RDWR | O_CREAT, 00644);
-
-    LOG(DCOPY_LOG_DBG, "Copying chunk of `%s' to `%s'.", op->operand, newfile);
-
     if(!outfd) {
-        LOG(DCOPY_LOG_ERR, "Unable to open %s", newfile);
+        LOG(DCOPY_LOG_ERR, "Unable to open `%s'. %s", \
+            new_file_path, strerror(errno));
         return;
     }
 
     if(fseek(in, DCOPY_CHUNK_SIZE * op->chunk, SEEK_SET) != 0) {
-        LOG(DCOPY_LOG_ERR, "Couldn't seek %s", op->operand);
-        perror("fseek");
+        LOG(DCOPY_LOG_ERR, "Couldn't seek `%s'. %s", \
+            op->operand, strerror(errno));
         return;
     }
 
-    size_t bytes = fread((void*)buf, 1, DCOPY_CHUNK_SIZE, in);
+    bytes_read = fread((void*)buf, 1, DCOPY_CHUNK_SIZE, in);
 
-    if(bytes <= 0) {
-        LOG(DCOPY_LOG_ERR, "Couldn't read %s", op->operand);
-        perror("fread");
+    if(bytes_read <= 0) {
+        LOG(DCOPY_LOG_ERR, "Couldn't read `%s'. %s", \
+            op->operand, strerror(errno));
         return;
     }
 
-    LOG(DCOPY_LOG_DBG, "Read %ld bytes.", bytes);
+    LOG(DCOPY_LOG_DBG, "Copy operation, we read `%zu' bytes.", bytes_read);
 
     lseek(outfd, DCOPY_CHUNK_SIZE * op->chunk, SEEK_SET);
-    int qty = write(outfd, buf, bytes);
+    bytes_written = write(outfd, buf, bytes_written);
 
-    if(qty > 0) {
-        DCOPY_statistics.total_bytes_copied += qty;
+    if(bytes_written > 0) {
+        DCOPY_statistics.total_bytes_copied += bytes_written;
     }
 
-    LOG(DCOPY_LOG_DBG, "Wrote %ld bytes (%ld total).", bytes, DCOPY_statistics.total_bytes_copied);
+    LOG(DCOPY_LOG_DBG, "Wrote %zu bytes (%ld total).", bytes_written, DCOPY_statistics.total_bytes_copied);
 
+    /*
+     * If the user is feeling brave, this is where we let them skip the
+     * comparison stage.
+     */
     if(!DCOPY_user_opts.skip_compare) {
         char* newop = DCOPY_encode_operation(CHECKSUM, op->chunk, op->operand, op->base_index);
         handle->enqueue(newop);
