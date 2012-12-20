1d8a1d347459897f5756e9c05278dfeaa2386010
Jon Bringhurst <jon@bringhurst.org>
2012-12-20 15:05:51 -0700

    Additional test for copying a random file over an empty file.

--- tests/test_dcp_single_file_to_single_file/test.sh
+++ tests/test_dcp_single_file_to_single_file/test.sh
@@ -18,6 +18,9 @@
 #   in the test runner (test_all.sh).
 ##############################################################################
 
+# Turn on verbose output
+#set -x
+
 # Print out the basic paths we'll be using.
 echo "Using dcp binary at: $DCP_TEST_BIN"
 echo "Using mpirun binary at: $DCP_MPIRUN_BIN"
@@ -73,6 +76,22 @@ fi
 
 if [[ "$MD5_C_EMPTY" != $(md5sum "$PATH_C_EMPTY" | /usr/bin/cut -f 2 -d "=") ]]; then
     echo "MD5 mismatch when copying empty file to empty file (C)."
+    exit 1
+fi
+
+##############################################################################
+# Test copying a random file to an empty file. The result should be two files
+# which both contain the contents of the first random file.
+
+$DCP_MPIRUN_BIN -np 3 $DCP_TEST_BIN $PATH_D_RANDOM $PATH_B_EMPTY > /dev/null 2>&1
+
+if [[ $? -ne 0 ]]; then
+    echo "Error returned when copying random file to empty file."
+    exit 1;
+fi
+
+if [[ "$MD5_D_RANDOM" != $(md5sum "$PATH_B_EMPTY" | /usr/bin/cut -f 2 -d "=") ]]; then
+    echo "MD5 mismatch when copying random file to empty file (B)."
     exit 1
 fi
 
