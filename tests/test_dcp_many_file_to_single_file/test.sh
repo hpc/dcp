fc3e3230e575e2b857d67dab15cddb4e7a890613
Jon Bringhurst <jon@bringhurst.org>
2013-01-03 09:29:41 -0700

    Finished implementation of many file to single file test.

--- tests/test_dcp_many_file_to_single_file/test.sh
+++ tests/test_dcp_many_file_to_single_file/test.sh
@@ -27,6 +27,7 @@ echo "Using tmp directory at: $DCP_TEST_TMP"
 # Generate the paths for:
 #   * Four files with zero length.
 #   * Four files which contain random data.
+#   * A file that doesn't exist.
 PATH_A_EMPTY="$DCP_TEST_TMP/dcp_test_many_file_to_single_file.$RANDOM.tmp"
 PATH_B_EMPTY="$DCP_TEST_TMP/dcp_test_many_file_to_single_file.$RANDOM.tmp"
 PATH_C_EMPTY="$DCP_TEST_TMP/dcp_test_many_file_to_single_file.$RANDOM.tmp"
@@ -35,6 +36,7 @@ PATH_E_RANDOM="$DCP_TEST_TMP/dcp_test_many_file_single_file.$RANDOM.tmp"
 PATH_F_RANDOM="$DCP_TEST_TMP/dcp_test_many_file_to_single_file.$RANDOM.tmp"
 PATH_G_RANDOM="$DCP_TEST_TMP/dcp_test_many_file_to_single_file.$RANDOM.tmp"
 PATH_H_RANDOM="$DCP_TEST_TMP/dcp_test_many_file_to_single_file.$RANDOM.tmp"
+PATH_I_NOEXIST="$DCP_TEST_TMP/dcp_test_many_file_to_single_file.$RANDOM.tmp"
 
 # Print out the generated paths to make debugging easier.
 echo "A_EMPTY  path at: $PATH_A_EMPTY"
@@ -58,24 +60,43 @@ dd if=/dev/urandom of=$PATH_F_RANDOM bs=3M count=3
 dd if=/dev/urandom of=$PATH_G_RANDOM bs=1M count=1
 dd if=/dev/urandom of=$PATH_H_RANDOM bs=6M count=2
 
-# FIXME: write the rest of this test.
-exit 1
-
 ##############################################################################
-# Test copying the directory to an empty file. The result should be an error.
+# Test copying several empty files into an empty file. This should result in
+# an error.
 
-$DCP_MPIRUN_BIN -np 3 $DCP_TEST_BIN $PATH_A_DIRECTORY $PATH_B_EMPTY
+$DCP_MPIRUN_BIN -np 3 $PATH_A_EMPTY $PATH_B_EMPTY $PATH_C_EMPTY $PATH_D_EMPTY
 if [[ $? -eq 0 ]]; then
-    echo "Unexpectedd success when copying a directory to an empty file. (A -> B)."
+    echo "Unexpected success when copying empty files to an empty file. (A,B,C -> D)."
     exit 1;
 fi
 
 ##############################################################################
-# Test copying a directory to a random file. The result should be an error.
+# Test copying several empty files into a random file. This should result in
+# an error.
 
-$DCP_MPIRUN_BIN -np 3 $DCP_TEST_BIN $PATH_A_DIRECTORY $PATH_C_RANDOM
+$DCP_MPIRUN_BIN -np 3 $PATH_A_EMPTY $PATH_B_EMPTY $PATH_C_EMPTY $PATH_E_EMPTY
 if [[ $? -eq 0 ]]; then
-    echo "Unexpected success when copying a directory to a random file (A -> C)."
+    echo "Unexpected success when copying empty files to a random file (A,B,C -> E)."
+    exit 1;
+fi
+
+##############################################################################
+# Test copying several random files into an empty file. This should result in
+# an error.
+
+$DCP_MPIRUN_BIN -np 3 $PATH_E_RANDOM $PATH_F_RANDOM $PATH_G_RANDOM $PATH_A_EMPTY
+if [[ $? -eq 0 ]]; then
+    echo "Unexpected success when copying random files to an empty file (E,F,G -> A)."
+    exit 1;
+fi
+
+##############################################################################
+# Test copying several random files into a file that doesn't exist. This
+# should result in an error.
+
+$DCP_MPIRUN_BIN -np 3 $PATH_E_RANDOM $PATH_F_RANDOM $PATH_G_RANDOM $PATH_I_NOEXIST
+if [[ $? -eq 0 ]]; then
+    echo "Unexpected success when copying random files to a no-exist file (E,F,G -> I)."
     exit 1;
 fi
 
