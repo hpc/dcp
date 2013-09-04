6ac5069f29d3332ebf0c8ae0b695b1ac42629999
Adam Moody <moody20@llnl.gov>
2013-09-04 10:08:33 -0700

    call bayer directory routines

--- src/treewalk.c
+++ src/treewalk.c
@@ -34,8 +34,6 @@ extern DCOPY_statistics_t DCOPY_statistics;
 /* given path, return level within directory tree */
 static int compute_depth(const char* path)
 {
-    /* TODO: ignore trailing '/' */
-
     const char* c;
     int depth = 0;
     for (c = path; *c != '\0'; c++) {
@@ -47,91 +45,22 @@ static int compute_depth(const char* path)
 }
 
 /**
- * This is the entry point for the "file stat stage". This function is called
- * from the jump table required for the main libcircle callbacks.
- */
-void DCOPY_do_treewalk(DCOPY_operation_t* op, \
-                       CIRCLE_handle* handle)
-{
-    struct stat64 statbuf;
-
-    if(lstat64(op->operand, &statbuf) < 0) {
-        LOG(DCOPY_LOG_DBG, "Could not get info for `%s'. errno=%d %s", op->operand, errno, strerror(errno));
-        DCOPY_retry_failed_operation(TREEWALK, handle, op);
-        return;
-    }
-
-    /* first check that we handle this file type */
-    if(! S_ISDIR(statbuf.st_mode) &&
-       ! S_ISREG(statbuf.st_mode) &&
-       ! S_ISLNK(statbuf.st_mode))
-    {
-        if (S_ISCHR(statbuf.st_mode)) {
-          LOG(DCOPY_LOG_ERR, "Encountered an unsupported file type S_ISCHR at `%s'.", op->operand);
-        } else if (S_ISBLK(statbuf.st_mode)) {
-          LOG(DCOPY_LOG_ERR, "Encountered an unsupported file type S_ISBLK at `%s'.", op->operand);
-        } else if (S_ISFIFO(statbuf.st_mode)) {
-          LOG(DCOPY_LOG_ERR, "Encountered an unsupported file type S_ISFIFO at `%s'.", op->operand);
-        } else if (S_ISSOCK(statbuf.st_mode)) {
-          LOG(DCOPY_LOG_ERR, "Encountered an unsupported file type S_ISSOCK at `%s'.", op->operand);
-        } else {
-          LOG(DCOPY_LOG_ERR, "Encountered an unsupported file type %x at `%s'.", statbuf.st_mode, op->operand);
-        }
-        return;
-    }
-
-    /* create new element to record file path and stat info */
-    DCOPY_stat_elem_t* elem = (DCOPY_stat_elem_t*) malloc(sizeof(DCOPY_stat_elem_t));
-    elem->file = strdup(op->dest_full_path);
-    elem->sb = (struct stat64*) malloc(sizeof(struct stat64));
-    elem->depth = compute_depth(op->dest_full_path);
-    memcpy(elem->sb, &statbuf, sizeof(struct stat64));
-    elem->next = NULL;
-
-    /* append element to tail of linked list */
-    if (DCOPY_list_head == NULL) {
-        DCOPY_list_head = elem;
-    }
-    if (DCOPY_list_tail != NULL) {
-        DCOPY_list_tail->next = elem;
-    }
-    DCOPY_list_tail = elem;
-
-    if(S_ISDIR(statbuf.st_mode)) {
-        /* LOG(DCOPY_LOG_DBG, "Stat operation found a directory at `%s'.", op->operand); */
-        DCOPY_stat_process_dir(op, &statbuf, handle);
-    }
-    else if(S_ISREG(statbuf.st_mode)) {
-        /* LOG(DCOPY_LOG_DBG, "Stat operation found a file at `%s'.", op->operand); */
-        DCOPY_stat_process_file(op, &statbuf, handle);
-    }
-    else if(S_ISLNK(statbuf.st_mode)) {
-        /* LOG(DCOPY_LOG_DBG, "Stat operation found a link at `%s'.", op->operand); */
-        DCOPY_stat_process_link(op, &statbuf, handle);
-    }
-    else {
-        LOG(DCOPY_LOG_ERR, "Encountered an unsupported file type %x at `%s'.", statbuf.st_mode, op->operand);
-        DCOPY_retry_failed_operation(TREEWALK, handle, op);
-        return;
-    }
-}
-
-/**
  * This function copies a link.
  */
-void DCOPY_stat_process_link(DCOPY_operation_t* op, \
+static void DCOPY_stat_process_link(DCOPY_operation_t* op,
                              const struct stat64* statbuf,
                              CIRCLE_handle* handle)
 {
     /* increment our link count by one */
     DCOPY_statistics.total_links++;
 
+    /* get source and destination paths */
     const char* src_path  = op->operand;
     const char* dest_path = op->dest_full_path;
 
     /* read link and terminate string with NUL character */
     char path[PATH_MAX + 1];
-    ssize_t rc = readlink(src_path, path, sizeof(path) - 1);
+    ssize_t rc = bayer_readlink(src_path, path, sizeof(path) - 1);
 
     if(rc < 0) {
         LOG(DCOPY_LOG_ERR, "Failed to read link `%s' readlink() errno=%d %s",
@@ -140,10 +69,11 @@ void DCOPY_stat_process_link(DCOPY_operation_t* op, \
         return;
     }
 
+    /* ensure that string ends with NUL */
     path[rc] = '\0';
 
     /* create new link */
-    int symrc = symlink(path, dest_path);
+    int symrc = bayer_symlink(path, dest_path);
 
     if(symrc < 0) {
         LOG(DCOPY_LOG_ERR, "Failed to create link `%s' symlink() errno=%d %s",
@@ -166,7 +96,7 @@ void DCOPY_stat_process_link(DCOPY_operation_t* op, \
  * This function inputs a file and creates chunk operations that get placed
  * onto the libcircle queue for future processing by the copy stage.
  */
-void DCOPY_stat_process_file(DCOPY_operation_t* op, \
+static void DCOPY_stat_process_file(DCOPY_operation_t* op,
                              const struct stat64* statbuf,
                              CIRCLE_handle* handle)
 {
@@ -195,7 +125,7 @@ void DCOPY_stat_process_file(DCOPY_operation_t* op, \
     * see makedev() to create valid dev */
     dev_t dev;
     memset(&dev, 0, sizeof(dev_t));
-    int mknod_rc = mknod(dest_path, DCOPY_DEF_PERMS_FILE | S_IFREG, dev);
+    int mknod_rc = bayer_mknod(dest_path, DCOPY_DEF_PERMS_FILE | S_IFREG, dev);
 
     if(mknod_rc < 0) {
         if(errno == EEXIST) {
@@ -240,20 +170,14 @@ void DCOPY_stat_process_file(DCOPY_operation_t* op, \
  * libcircle operations for every object in the directory. It then places those
  * operations on the libcircle queue and returns.
  */
-void DCOPY_stat_process_dir(DCOPY_operation_t* op,
+static void DCOPY_stat_process_dir(DCOPY_operation_t* op,
                             const struct stat64* statbuf,
                             CIRCLE_handle* handle)
 {
-    DIR* curr_dir;
-    char* curr_dir_name;
-    char* newop;
-
-    struct dirent* curr_ent;
-    char newop_path[PATH_MAX];
-
     /* increment our directory count by one */
     DCOPY_statistics.total_dirs++;
 
+    /* get destination path */
     const char* dest_path = op->dest_full_path;
 
     /* first, create the destination directory */
@@ -271,9 +195,10 @@ void DCOPY_stat_process_dir(DCOPY_operation_t* op,
     }
 
     /* iterate through source directory and add items to queue */
-    curr_dir = opendir(op->operand);
+    DIR* curr_dir = bayer_opendir(op->operand);
 
     if(curr_dir == NULL) {
+        /* failed to open directory */
         LOG(DCOPY_LOG_ERR, "Unable to open dir `%s'. errno=%d %s", \
             op->operand, errno, strerror(errno));
 
@@ -281,19 +206,21 @@ void DCOPY_stat_process_dir(DCOPY_operation_t* op,
         return;
     }
     else {
-        while((curr_ent = readdir(curr_dir)) != NULL) {
-            curr_dir_name = curr_ent->d_name;
+        struct dirent* curr_ent;
+        while((curr_ent = bayer_readdir(curr_dir)) != NULL) {
+            char* curr_dir_name = curr_ent->d_name;
 
             /* We don't care about . or .. */
             if((strncmp(curr_dir_name, ".", 2)) && (strncmp(curr_dir_name, "..", 3))) {
 
                 /* build new object name */
+                char newop_path[PATH_MAX];
                 sprintf(newop_path, "%s/%s", op->operand, curr_dir_name);
 
                 LOG(DCOPY_LOG_DBG, "Stat operation is enqueueing `%s'", newop_path);
 
                 /* Distributed recursion here. */
-                newop = DCOPY_encode_operation(TREEWALK, 0, newop_path, \
+                char* newop = DCOPY_encode_operation(TREEWALK, 0, newop_path, \
                                                op->source_base_offset, op->dest_base_appendix, op->file_size);
                 handle->enqueue(newop);
 
@@ -302,8 +229,83 @@ void DCOPY_stat_process_dir(DCOPY_operation_t* op,
         }
     }
 
-    closedir(curr_dir);
+    bayer_closedir(curr_dir);
+
     return;
+}
+
+/**
+ * This is the entry point for the "file stat stage". This function is called
+ * from the jump table required for the main libcircle callbacks.
+ */
+void DCOPY_do_treewalk(DCOPY_operation_t* op,
+                       CIRCLE_handle* handle)
+{
+    struct stat64 statbuf;
+
+    /* stat the item */
+    if(bayer_lstat64(op->operand, &statbuf) < 0) {
+        LOG(DCOPY_LOG_DBG, "Could not get info for `%s'. errno=%d %s", op->operand, errno, strerror(errno));
+        DCOPY_retry_failed_operation(TREEWALK, handle, op);
+        return;
+    }
+
+    /* first check that we handle this file type */
+    if(! S_ISDIR(statbuf.st_mode) &&
+       ! S_ISREG(statbuf.st_mode) &&
+       ! S_ISLNK(statbuf.st_mode))
+    {
+        if (S_ISCHR(statbuf.st_mode)) {
+          LOG(DCOPY_LOG_ERR, "Encountered an unsupported file type S_ISCHR at `%s'.", op->operand);
+        } else if (S_ISBLK(statbuf.st_mode)) {
+          LOG(DCOPY_LOG_ERR, "Encountered an unsupported file type S_ISBLK at `%s'.", op->operand);
+        } else if (S_ISFIFO(statbuf.st_mode)) {
+          LOG(DCOPY_LOG_ERR, "Encountered an unsupported file type S_ISFIFO at `%s'.", op->operand);
+        } else if (S_ISSOCK(statbuf.st_mode)) {
+          LOG(DCOPY_LOG_ERR, "Encountered an unsupported file type S_ISSOCK at `%s'.", op->operand);
+        } else {
+          LOG(DCOPY_LOG_ERR, "Encountered an unsupported file type %x at `%s'.", statbuf.st_mode, op->operand);
+        }
+        return;
+    }
+
+    /* create new element to record file path and stat info */
+    DCOPY_stat_elem_t* elem = (DCOPY_stat_elem_t*) bayer_malloc(
+        sizeof(DCOPY_stat_elem_t), "Linked list element", __FILE__, __LINE__);
+    elem->file = strdup(op->dest_full_path);
+    elem->sb = (struct stat64*) bayer_malloc(
+        sizeof(struct stat64), "Stat buffer", __FILE__, __LINE__);
+    elem->depth = compute_depth(op->dest_full_path);
+    memcpy(elem->sb, &statbuf, sizeof(struct stat64));
+    elem->next = NULL;
+
+    /* append element to tail of linked list */
+    if (DCOPY_list_head == NULL) {
+        DCOPY_list_head = elem;
+    }
+    if (DCOPY_list_tail != NULL) {
+        DCOPY_list_tail->next = elem;
+    }
+    DCOPY_list_tail = elem;
+
+    /* handle item depending on its type */
+    if(S_ISDIR(statbuf.st_mode)) {
+        /* LOG(DCOPY_LOG_DBG, "Stat operation found a directory at `%s'.", op->operand); */
+        DCOPY_stat_process_dir(op, &statbuf, handle);
+    }
+    else if(S_ISREG(statbuf.st_mode)) {
+        /* LOG(DCOPY_LOG_DBG, "Stat operation found a file at `%s'.", op->operand); */
+        DCOPY_stat_process_file(op, &statbuf, handle);
+    }
+    else if(S_ISLNK(statbuf.st_mode)) {
+        /* LOG(DCOPY_LOG_DBG, "Stat operation found a link at `%s'.", op->operand); */
+        DCOPY_stat_process_link(op, &statbuf, handle);
+    }
+    else {
+        LOG(DCOPY_LOG_ERR, "Encountered an unsupported file type %x at `%s'.", statbuf.st_mode, op->operand);
+        DCOPY_retry_failed_operation(TREEWALK, handle, op);
+        return;
+    }
 }
 
 /* EOF */
--- src/treewalk.h
+++ src/treewalk.h
@@ -5,19 +5,7 @@
 
 #include "common.h"
 
-void DCOPY_do_treewalk(DCOPY_operation_t* op, \
+void DCOPY_do_treewalk(DCOPY_operation_t* op,
                        CIRCLE_handle* handle);
-
-void DCOPY_stat_process_link(DCOPY_operation_t* op, \
-                             const struct stat64* statbuf,
-                             CIRCLE_handle* handle);
-
-void DCOPY_stat_process_file(DCOPY_operation_t* op, \
-                             const struct stat64* statbuf,
-                             CIRCLE_handle* handle);
-
-void DCOPY_stat_process_dir(DCOPY_operation_t* op,
-                            const struct stat64* statbuf,
-                            CIRCLE_handle* handle);
 
 #endif /* __DCP_TREEWALK_H */
